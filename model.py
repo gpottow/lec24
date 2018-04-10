99fe1906d02c6c6359156bfc6dcf313c5315c4a1
gpottow <gpottow@umich.edu>
2018-04-10 09:59:25 -0400

    completed lecture exercise

--- model.py
+++ model.py
@@ -5,9 +5,31 @@ BB_FILE_NAME = 'umbball.csv'
 
 bb_seasons = []
 
-def init_bball(csv_file_name):
-    pass
+def init_bball(csv_file_name=BB_FILE_NAME):
+    global bb_seasons
+    with open(csv_file_name) as csvfile:
+        bb_reader = csv.reader(csvfile)
+        next(bb_reader) #remove headers
+        next(bb_reader) #remove headers
+        bb_seasons = [] #empty strings
+        for row in bb_reader:
+            row[3] = int(row[3])
+            row[4] = int(row[4])
+            row[5] = float(row[5])
+            bb_seasons.append(row)
+    return bb_seasons
+
 
 def get_bball_seasons(sortby='year', sortorder='desc'):
-    global bb_seasons
-    return bb_seasons
+    if sortby == 'year':
+        sortcol = 1
+    elif sortby == 'wins':
+        sortcol = 3
+    elif sortby == 'pct':
+        sortcol = 5
+    else:
+        sortcol = 0
+
+    rev = (sortorder == 'desc')
+    sorted_list = sorted(bb_seasons, key=lambda row: row[sortcol], reverse=rev)
+    return sorted_list
